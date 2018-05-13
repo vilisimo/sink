70718d47257aae9d519076c74516c9e1b2427c0e
Simonas Viliunas <vilisimo@gmail.com>
2018-05-13 22:49:15 +0300

    Add non negative double value validation

--- lib/utils/validations.dart
+++ lib/utils/validations.dart
@@ -5,3 +5,11 @@ notEmpty(String value) {
     throw new InvalidInput('Field cannot be empty');
   }
 }
+
+nonNegative(String value) {
+  notEmpty(value);
+
+  if (double.parse(value) <= 0) {
+    throw new InvalidInput('Value must be non-negative');
+  }
+}
--- test/utils/validations_test.dart
+++ test/utils/validations_test.dart
@@ -18,4 +18,24 @@ void main() {
   test('does allow a valid string', () {
     notEmpty('Valid');
   });
+
+  test('does not allow an empty string instead of a number', () {
+    expect(() => nonNegative(''), throwsA(new isInstanceOf<InvalidInput>()));
+  });
+
+  test('does not allow whitespaces in a number', () {
+    expect(() => nonNegative('   '), throwsA(new isInstanceOf<InvalidInput>()));
+  });
+
+  test('does not allow a null instead of a number', () {
+    expect(() => nonNegative(null), throwsA(new isInstanceOf<InvalidInput>()));
+  });
+  
+  test('does not allow a negative number', () {
+    expect(() => nonNegative('-1'), throwsA(new isInstanceOf<InvalidInput>()));
+  });
+
+  test('allows a valid number', () {
+    expect(() => nonNegative('0'), throwsA(new isInstanceOf<InvalidInput>()));
+  });
 }
