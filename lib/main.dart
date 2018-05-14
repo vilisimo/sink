1db977b4bda8bdea7c34b97926e4bc1c08c05f27
Simonas Viliunas <vilisimo@gmail.com>
2018-05-14 21:47:49 +0300

    Move form to a new page

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       appBar: new AppBar(
         title: Container(
           child: new Text('Sink'),
@@ -32,9 +32,26 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ExpenseForm(),
+            Text('Please enter a new expense'),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add an expense',
+        child: new Icon(Icons.add),
+        onPressed: () => _enterExpense(context)
+      ),
+    );
+  }
+
+  void _enterExpense(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        fullscreenDialog: false,
+        builder: (context) {
+          return ExpenseForm();
+        }
       ),
     );
   }
--- lib/ui/expense_form.dart
+++ lib/ui/expense_form.dart
@@ -3,7 +3,6 @@ import 'package:sink/utils/validations.dart';
 import 'package:sink/exceptions/InvalidInput.dart';
 
 class ExpenseForm extends StatefulWidget {
-
   @override
   ExpenseFormState createState() {
     return ExpenseFormState();
@@ -11,7 +10,6 @@ class ExpenseForm extends StatefulWidget {
 }
 
 class ExpenseFormState extends State<ExpenseForm> {
-
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   String _description;
@@ -20,54 +18,57 @@ class ExpenseFormState extends State<ExpenseForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 50.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(labelText: "Enter a price"),
-              keyboardType: TextInputType.number,
-              autofocus: true,
-              validator: (value) => _validatePrice(value),
-              onSaved: (value) => _cost = double.parse(value),
+    return Scaffold(
+      appBar: AppBar(title: Text('Enter a record')),
+      body: new Builder(
+        builder: (BuildContext context) {
+          return Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Enter a price"),
+                  keyboardType: TextInputType.number,
+                  validator: (value) => _validatePrice(value),
+                  onSaved: (value) => _cost = double.parse(value),
+                ),
+                _textFormField("Add a note", (value) => _description = value),
+                _textFormField(
+                    "Enter a category", (value) => _category = value),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Scaffold.of(context).showSnackBar(
+                            new SnackBar(content: new Text('Processing...')));
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: new Text('Submit'),
+                  ),
+                ),
+              ],
             ),
-            _textFormField("Add a note", (value) => _description = value),
-            _textFormField("Enter a category", (value) => _category = value),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    Scaffold.of(context).showSnackBar(
-                        new SnackBar(content: new Text('Processing...')));
-                  }
-                },
-                child: new Text('Submit'),
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
 
   TextFormField _textFormField(String label, Function onSave) {
     return TextFormField(
-            decoration: InputDecoration(labelText: label),
-            validator: (value) => _validateNotEmpty(value),
-            onSaved: onSave,
-          );
+      decoration: InputDecoration(labelText: label),
+      validator: (value) => _validateNotEmpty(value),
+      onSaved: onSave,
+    );
   }
 
   String _validateNotEmpty(String value) {
     try {
       notEmpty(value);
       return null;
-    } on InvalidInput catch(e) {
+    } on InvalidInput catch (e) {
       return e.cause;
     }
   }
@@ -76,7 +77,7 @@ class ExpenseFormState extends State<ExpenseForm> {
     try {
       nonNegative(value);
       return null;
-    } on InvalidInput catch(e) {
+    } on InvalidInput catch (e) {
       return e.cause;
     }
   }
