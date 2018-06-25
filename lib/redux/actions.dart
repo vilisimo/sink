bff5e0840d164f4d8eb11c9ddc769437591dddc5
Simonas Viliunas <vilisimo@gmail.com>
2018-06-25 19:52:55 +0300

    Mark actions as immutable

--- lib/redux/actions.dart
+++ lib/redux/actions.dart
@@ -1,11 +1,14 @@
+import 'package:meta/meta.dart';
 import 'package:sink/domain/entry.dart';
 
+@immutable
 class AddEntry {
   final Entry entry;
 
   AddEntry(this.entry);
 }
 
+@immutable
 class EditEntry {
   final Entry entry;
   final int index;
@@ -13,6 +16,7 @@ class EditEntry {
   EditEntry(this.entry, this.index);
 }
 
+@immutable
 class DeleteEntry {
   final Entry entry;
 
