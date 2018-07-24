05f81fec08644faea154055d75031f06aa570b3d
Simonas Viliunas <vilisimo@gmail.com>
2018-07-24 22:33:59 +0300

    Add entry test

---
+++ test/models/entry_test.dart
@@ -0,0 +1,14 @@
+import 'package:sink/models/entry.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('returns an empty entry', () {
+    final Entry entry = Entry.empty();
+
+    expect(entry.id, isNotNull);
+    expect(entry.date, isNotNull);
+    expect(entry.cost, isNull);
+    expect(entry.description, '');
+    expect(entry.category, '');
+  });
+}
