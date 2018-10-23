e36fac50e1ded56f6d3cf01c4fd4640b3f9ad9c6
Simonas Viliunas <vilisimo@gmail.com>
2018-10-23 21:29:18 +0300

    Format code

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -30,49 +30,49 @@ class _HomeScreenState extends State<HomeScreen>
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        key: _scaffoldKey,
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).backgroundColor,
-          title: Container(
-            child: Text('Sink'),
-            alignment: Alignment.center,
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).backgroundColor,
+        title: Container(
+          child: Text('Sink'),
+          alignment: Alignment.center,
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          EntriesPage(),
+          Center(child: Text('Placeholder for statistics')),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add an expense',
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        child: Icon(Icons.add),
+        onPressed: () => showDialog(
+            context: context, builder: (context) => AddExpensePage()),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 5.0,
+        clipBehavior: Clip.antiAlias,
+        shape: CircularNotchedRectangle(),
+        child: Material(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          child: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.receipt)),
+              Tab(icon: Icon(Icons.insert_chart)),
+            ],
+            controller: _tabController,
+            labelColor: Colors.black,
+            indicatorColor: Colors.white,
+            unselectedLabelColor: Colors.blueGrey,
           ),
         ),
-        body: TabBarView(
-          controller: _tabController,
-          children: <Widget>[
-            EntriesPage(),
-            Center(child: Text('Placeholder for statistics')),
-          ],
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          tooltip: 'Add an expense',
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          child: Icon(Icons.add),
-          onPressed: () => showDialog(
-              context: context, builder: (context) => AddExpensePage()),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          notchMargin: 5.0,
-          clipBehavior: Clip.antiAlias,
-          shape: CircularNotchedRectangle(),
-          child: Material(
-            color: Theme.of(context).scaffoldBackgroundColor,
-            child: TabBar(
-              tabs: <Widget>[
-                Tab(icon: Icon(Icons.receipt)),
-                Tab(icon: Icon(Icons.insert_chart)),
-              ],
-              controller: _tabController,
-              labelColor: Colors.black,
-              indicatorColor: Colors.white,
-              unselectedLabelColor: Colors.blueGrey,
-            ),
-          ),
-        ),
+      ),
     );
   }
 }
