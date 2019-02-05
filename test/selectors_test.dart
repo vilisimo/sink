4ad5ab89fa4921d6e2b261ee5b8631a29e803eed
vilisimo <vilisimo@gmail.com>
2019-02-05 21:17:00 +0200

    Test retrieval of categories from state

--- test/selectors_test.dart
+++ test/selectors_test.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:sink/models/category.dart';
 import 'package:sink/models/entry.dart';
-import 'package:sink/redux/state.dart';
 import 'package:sink/redux/selectors.dart';
+import 'package:sink/redux/state.dart';
 import 'package:test/test.dart';
 
 main() {
@@ -13,5 +15,15 @@ main() {
     var entry = getLastRemoved(state);
 
     expect(entry, state.removed[1]);
+  });
+
+  test('retrieves a list of categories', () {
+    var category = Category(id: "1", name: "category", color: Colors.red);
+    var state = AppState(categories: Set<Category>.from([category]));
+
+    var categories = getCategories(state);
+
+    expect(categories.length, equals(1));
+    expect(categories.first, equals(category));
   });
 }
