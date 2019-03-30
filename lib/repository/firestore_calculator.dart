aa46b5e2ee860c85f0a7b6b2d68f35d115f02b5d
vilisimo <vilisimo@gmail.com>
2019-03-30 20:16:10 +0200

    Do not include income in expense calculations

--- lib/repository/firestore_calculator.dart
+++ lib/repository/firestore_calculator.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sink/models/entry.dart';
 
-double totalCost(List<DocumentSnapshot> snapshots) =>
-  snapshots.fold(0.0, (prev, next) => prev + next['cost']);
+double totalCost(List<DocumentSnapshot> snapshots) => snapshots
+    .where((el) => el['type'] == EntryType.EXPENSE.index)
+    .fold(0.0, (prev, next) => prev + next['cost']);
