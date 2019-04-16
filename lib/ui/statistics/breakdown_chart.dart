657830d77350a739bbbb92ab0139dca5fd778788
vilisimo <vilisimo@gmail.com>
2019-04-16 22:00:48 +0300

    Move dot component to ui package

--- lib/ui/statistics/breakdown_chart.dart
+++ lib/ui/statistics/breakdown_chart.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sink/common/dot.dart';
+import 'package:sink/ui/common/dot.dart';
 import 'package:sink/ui/statistics/charts.dart';
 
 class SortedBreakdown extends StatelessWidget {
