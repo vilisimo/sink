2b70d894c68b0cda0a02890e06ec38db9fa1bb60
vilisimo <vilisimo@gmail.com>
2019-08-29 16:28:13 +0300

    Add loading button

--- lib/ui/common/buttons.dart
+++ lib/ui/common/buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sink/theme/palette.dart' as Palette;
 
 class RoundedButton extends StatelessWidget {
   final Function onPressed;
@@ -29,8 +30,38 @@ class RoundedButton extends StatelessWidget {
       child: FlatButton(
         shape: RoundedRectangleBorder(borderRadius: radius),
         child: Text(text, style: textStyle),
+        disabledColor: Colors.grey,
         color: buttonColor,
-        onPressed: () => onPressed(),
+        onPressed: onPressed != null ? () => onPressed() : null,
+      ),
+    );
+  }
+}
+
+class LoadingButton extends StatelessWidget {
+  final BorderRadius radius;
+  final Color buttonColor;
+
+  LoadingButton({radius, buttonColor})
+      : this.radius = radius ?? BorderRadius.circular(5.0),
+        this.buttonColor = buttonColor ?? Colors.lightGreen;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
+      child: FlatButton(
+        shape: RoundedRectangleBorder(borderRadius: radius),
+        child: SizedBox(
+          width: 16.0,
+          height: 16.0,
+          child: CircularProgressIndicator(
+            strokeWidth: 3.0,
+          ),
+        ),
+        color: buttonColor,
+        disabledColor: Palette.disabled,
+        onPressed: null,
       ),
     );
   }
