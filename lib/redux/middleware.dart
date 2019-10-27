81b6b2c9d0c74195d06e1cb2e1e2ee0c25923b92
vilisimo <vilisimo@gmail.com>
2019-10-27 15:10:39 +0200

    Extract functionality into methods

--- lib/redux/middleware.dart
+++ lib/redux/middleware.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:redux/redux.dart';
 import 'package:sink/common/auth.dart';
 import 'package:sink/models/category.dart';
@@ -12,28 +14,12 @@ class SinkMiddleware extends MiddlewareClass<AppState> {
   @override
   void call(Store<AppState> store, dynamic action, NextDispatcher next) async {
     if (action is RetrieveUser) {
-      auth.getCurrentUser().then((user) {
-        if (user != null && user.isEmailVerified) {
-          final userId = user.uid.toString();
-          store.dispatch(SetUserDetails(id: userId, email: user.email));
-          store.dispatch(InitializeDatabase(user.uid));
-          store.dispatch(RehydrateState());
-        } else {
-          store.dispatch(SetUserDetails(id: "", email: ""));
-        }
-      });
+      auth.getCurrentUser().then((user) => attemptUserRetrieval(store, user));
     } else if (action is SignIn) {
-      auth.signIn(action.email, action.password).then((user) {
-        if (user.isEmailVerified) {
-          store.dispatch(SetUserDetails(id: user.uid, email: user.email));
-          store.dispatch(InitializeDatabase(user.uid));
-          store.dispatch(ReportSignInSuccess());
-          store.dispatch(RehydrateState());
-        } else {
-          store.dispatch(SignOut());
-          store.dispatch(ReportAuthenticationError("Email is not verified."));
-        }
-      }).catchError((e) => store.dispatch(ReportAuthenticationError(e.code)));
+      auth
+          .signIn(action.email, action.password)
+          .then((user) => attemptSignIn(store, user))
+          .catchError((e) => store.dispatch(ReportAuthenticationError(e.code)));
     } else if (action is SignOut) {
       auth
           .signOut()
@@ -52,23 +38,11 @@ class SinkMiddleware extends MiddlewareClass<AppState> {
       database.categories
           .orderBy('name', descending: false)
           .snapshots()
-          .listen((qs) {
-        Set<Category> categories = Set();
-        qs.documents.forEach((ds) => categories.add(Category.fromSnapshot(ds)));
-        store.dispatch(ReloadCategories(categories));
-        store.dispatch(ReloadColors(categories.map((c) => c.color).toSet()));
-      });
+          .listen((qs) => reloadCategories(store, qs));
       store.dispatch(LoadFirstEntry());
     } else if (action is LoadFirstEntry) {
       final database = getRepository(store.state);
-      database.getFirstEntry().listen((event) {
-        if (event.documents.isNotEmpty) {
-          var firstEntry = Entry.fromSnapshot(event.documents.first).date;
-          store.dispatch(LoadMonths(firstEntry, DateTime.now()));
-        } else {
-          store.dispatch(LoadMonths(DateTime.now(), DateTime.now()));
-        }
-      });
+      database.getFirstEntry().listen((event) => loadMonths(store, event));
     } else if (action is AddEntry) {
       final database = getRepository(store.state);
       database.create(action.entry);
@@ -88,5 +62,44 @@ class SinkMiddleware extends MiddlewareClass<AppState> {
     }
 
     next(action);
+  }
+
+  void attemptUserRetrieval(Store<AppState> store, FirebaseUser user) {
+    if (user != null && user.isEmailVerified) {
+      final userId = user.uid.toString();
+      store.dispatch(SetUserDetails(id: userId, email: user.email));
+      store.dispatch(InitializeDatabase(user.uid));
+      store.dispatch(RehydrateState());
+    } else {
+      store.dispatch(SetUserDetails(id: "", email: ""));
+    }
+  }
+
+  void attemptSignIn(Store<AppState> store, FirebaseUser user) {
+    if (user.isEmailVerified) {
+      store.dispatch(SetUserDetails(id: user.uid, email: user.email));
+      store.dispatch(InitializeDatabase(user.uid));
+      store.dispatch(ReportSignInSuccess());
+      store.dispatch(RehydrateState());
+    } else {
+      store.dispatch(SignOut());
+      store.dispatch(ReportAuthenticationError("Email is not verified."));
+    }
+  }
+
+  void reloadCategories(Store<AppState> store, QuerySnapshot event) {
+    Set<Category> categories = Set();
+    event.documents.forEach((ds) => categories.add(Category.fromSnapshot(ds)));
+    store.dispatch(ReloadCategories(categories));
+    store.dispatch(ReloadColors(categories.map((c) => c.color).toSet()));
+  }
+
+  void loadMonths(Store<AppState> store, QuerySnapshot event) {
+    if (event.documents.isNotEmpty) {
+      var firstEntry = Entry.fromSnapshot(event.documents.first).date;
+      store.dispatch(LoadMonths(firstEntry, DateTime.now()));
+    } else {
+      store.dispatch(LoadMonths(DateTime.now(), DateTime.now()));
+    }
   }
 }
